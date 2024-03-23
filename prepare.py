b99e8a28035fcc95d37bc23e64e8e3dfe5e0b0ce
kimzz14 <kimzz1004@gmail.com>
2024-03-23 06:22:15 +0000

    model reverse

--- prepare.py
+++ prepare.py
@@ -54,9 +54,10 @@ command_GAPIT = """options(repos = c(CRAN = "https://cloud.r-project.org/"))
 source("../../GAPIT_v3.4/gapit_functions.txt")
 myY <- read.table("phenotype.txt", head=TRUE)
 myG <- read.table("genotype.hmp.txt", head=FALSE)
-myGAPIT <- GAPIT(Y=myY, G=myG, PCA.total=3, model=c("Blink", "GLM", "MLM", "CMLM", "MLMM","SUPER", "FarmCPU", "gBLUP", "cBLUP", "sBLUP"),Multiple_analysis=TRUE, Inter.Plot=TRUE)
+myGAPIT <- GAPIT(Y=myY, G=myG, PCA.total=3, model=c("Blink", "FarmCPU", "gBLUP", "sBLUP", "cBLUP", "SUPER", "MLMM", "CMLM", "MLM", "GLM"),Multiple_analysis=TRUE, Inter.Plot=TRUE)
 q()
 """
+
 
 prefix = 'GAPIT'
 if os.path.isdir(prefix) == True:
